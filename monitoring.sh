a6c23ed3e480fc04d75bf7f92317bba3821f9ed4
Pierre Pontet <ppontet@student.42lyon.fr>
2024-12-06 19:07:18 +0100

    EOD WIP

--- monitoring.sh
+++ monitoring.sh
@@ -21,13 +21,21 @@ VIRTUAL_CPU=$(cat /proc/cpuinfo | grep "processor" | uniq | awk '{print $3 + 1}'
 
 RAM_TOTAL=$(free --mega | grep "Mem" | awk '{print $2}')
 RAM_USED=$(free --mega | grep "Mem" | awk '{print $3}')
-RAM_PERCENT=$(free --mega | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
+# RAM_PERCENT=$(free --mega | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
+RAM_PERCENT=$(echo $RAM_USED $RAM_TOTAL | awk '{printf("%.2f"), $1/$2 * 100}')
 
 DISK_USED=$(df -B 1000000 --total | grep "total" | awk '{print $3}')
 DISK_TOTAL=$(df -B 1000000000 --total | grep "total" | awk '{print $2}')
 DISK_PERCENT=$(df -B 1 --total | grep "total" | awk '{printf("%.2f"), $3/$2*100}')
 
+export LANG=C
+# CPU_LOAD=$(top -bn1 | grep '%Cpu' | sed 's/,/ /g' | awk '{print 100 - $8}')
+CPU_LOAD=$(top -bn1 | grep '^%Cpu' | cut -c 36-40 | awk '{printf("%.1f%%"), 100 - $1}')
+# CPU_LOAD=$(awk '/cpu / {usage=($2+$4)*100/($2+$4+$5)} END {printf("%.2f%%"),  usage "%"}' /pro>
 
+LAST_BOOT=$(who -b | awk '{print($3 " " $4)}')
+
+LVM_USE=$(if [ $(lsblk | grep lvm | wc -l) -eq 8 ]; then echo no; else echo yes; fi)
 
 echo "  #Architecture: $ARCH
         #CPU physical: $PHYSICAL_CPU
@@ -41,5 +49,4 @@ echo "  #Architecture: $ARCH
         #User log: $USER_LOG
         #Network: IP $IP ($MAC)
         #Sudo: $SUDO_CMDS cmd"
-
 
